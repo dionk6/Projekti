2aaac9a962cdc8c00b3980c8804ce934a40d90d4
dion kuka <kukadion1@gmail.com>
2020-05-17 17:48:57 +0200

    slider commit

--- javascript/home.js
+++ javascript/home.js
@@ -3,3 +3,32 @@ function Buy(title){
     var queryString ='?'+title;
     window.location.href='product.html'+queryString;
 }
+
+
+function right(){
+    var slides = document.getElementsByClassName('slide');
+    var index =0;
+    for(var slide of slides){
+        index++;
+        if(slide.classList.contains('show-slide') && index<3){
+            slide.classList.remove('show-slide');
+            slide.classList.add('remove-slide');
+            slide.nextElementSibling.classList.remove('remove-slide');
+            slide.nextElementSibling.classList.add('show-slide');
+            break;
+        }
+    }
+}
+
+function left(){
+    var slides = document.getElementsByClassName('slide');
+    for(var i=0 ;i<slides.length;i++){
+        if(slides[i].classList.contains('show-slide') && i!=0){
+            slides[i].classList.remove('show-slide');
+            slides[i].classList.add('remove-slide');
+            slides[i].previousElementSibling.classList.remove('remove-slide');
+            slides[i].previousElementSibling.classList.add('show-slide');
+            break;
+        }
+    }
+}
