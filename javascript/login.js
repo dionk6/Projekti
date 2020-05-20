451cea2a8f0f1237452cf5cb72b0ac7a643a85b4
dion kuka <kukadion1@gmail.com>
2020-05-20 17:50:54 +0200

    login form validation

--- javascript/login.js
+++ javascript/login.js
@@ -4,16 +4,58 @@ function SignUp(){
 }
 
 function backToLogin(){
-    username = document.getElementById('usernameSignupInput').value;
-    var pass = document.getElementById('passwordSignupInput').value;
-    var inputConfirmPassword = document.getElementById('ConfirmPasswordInput').value;
-    if(pass == inputConfirmPassword){
-        password=pass;
-    } else {
-        alert("Password not matching");
+    //------------Full Name Validation
+    var fullName = "";
+    if(document.getElementById('fullnameInput').value.trim()==""){
+        alert("Please write your Name");
+    }else {
+        fullName=document.getElementById('fullnameInput').value;
     }
-    document.getElementById('login').style.display="block";
-    document.getElementById('signup').style.display="none";
+    //-------------------------------
+
+    //-----------Email validation
+    var email="";
+    var reg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    if(document.getElementById("emailInput").value.trim()==""){
+        alert("Please write your email");
+    } else {
+        var docu = document.getElementById("emailInput").value;
+        if(reg.test(docu) == false ){
+            alert("Invalid Email Address");
+        } else {
+            email=docu;
+        }
+    }
+    //------------------------------
+
+    //-------------Username Validation
+    if(document.getElementById('usernameSignupInput').value.trim() == ""){
+        alert("Please write your Username");
+    } else {
+        username = document.getElementById('usernameSignupInput').value;
+    }
+    //----------------------------------
+
+    //-------------Password Validation
+    if(document.getElementById('passwordSignupInput').value.trim() == "" || document.getElementById('ConfirmPasswordInput').value.trim()==""){
+        alert("Please write your password");
+    } else {
+        var pass = document.getElementById('passwordSignupInput').value;
+        var inputConfirmPassword = document.getElementById('ConfirmPasswordInput').value;
+        if(pass == inputConfirmPassword){
+            password=pass;
+        } else {
+            alert("Password not matching");
+        }
+    }
+    //------------------------------
+
+    //-------------Create account
+    if(username != "" && password != ""){
+        document.getElementById('login').style.display="block";
+        document.getElementById('signup').style.display="none";
+    }
+    //-----------------------------
 }
 
 var username = '';
@@ -23,7 +65,7 @@ function OpenHomePage(){
     var inputUsername = document.getElementById('usernameInput').value;
     var inputPassword = document.getElementById('passwordInput').value;
     if(inputUsername=='' || inputPassword==''){
-        alert("Please write your username and password");
+        alert("Please write your username or password");
     }else if(inputUsername==username && inputPassword==password){
         window.location.href="home.html"
     } else {
